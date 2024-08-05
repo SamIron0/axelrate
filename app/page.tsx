122bfd9458241619c91be08ca8d60a6f0ff9d4a7
SamIron0 <samuelironkwec@gmail.com>
2024-08-05 14:05:07 -0500

    x

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,21 @@
+"use client";
+import "next-cloudinary/dist/cld-video-player.css";
 import { upload } from "@/lib/cloudinary/cloudinary";
 import { useFormState } from "react-dom";
 import MapWrapper from "@/components/MapWrapper";
-import Map from "@/components/Map";
 export default function Home() {
   const [url, formAction] = useFormState(upload, null);
   return (
-    <div className="min-h-screen flex-col items-center gap-4 p-8 justify-between ">
-      <h1>Protest Map</h1>
+    <div className="min-h-screen flex-col items-center justify-between ">
+      <div className="flex justify-center my-10 items-center">
+        <form action={formAction}>
+          <input type="file" name="video" accept="video/*" />
+          <button className="bg-blue-800 text-white p-2 rounded-md">
+            Upload
+          </button>
+        </form>
+      </div>
+     
       <MapWrapper />
     </div>
   );
