361087cbdfb9cead36cf21493ed3655b5ef4cc09
SamIron0 <samuelironkwec@gmail.com>
2024-08-05 16:29:14 -0500

    x

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,11 +1,11 @@
 import { createServerClient, type CookieOptions } from "@supabase/ssr";
 import { cookies } from "next/headers";
-import { Database } from "@/supabase/db_types";
+import { Database } from "./types";
 
 export const createClient = (cookieStore: ReturnType<typeof cookies>) => {
   return createServerClient<Database>(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    process.env.PUBLIC_SUPABASE_URL!,
+    process.env.PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
         get(name: string) {
